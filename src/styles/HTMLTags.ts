5f9621022edeea1b4d376465e54cc0dfcb35db44
AtomicFeasT <atomic-feast@hotmail.com>
2022-10-17 16:30:25 -0300

    Added text tags sizes and the reset

---
+++ src/styles/HTMLTags.ts
@@ -0,0 +1,17 @@
+export const setTags = {
+  root: {
+    "--H1Mobile": "$2xl",
+    "--H2Mobile": "$4xl",
+    "--H3Mobile": "$5xl",
+    "--PMobile": "$xs",
+
+    "--H1Tablet": "$5xl",
+    "--H2Tablet": "$4xl",
+    "--H3Tablet": "$2xl",
+    "--PTablet": "$md",
+
+    "--H1Desktop": "$6xl",
+    "--H2Desktop": "$5xl",
+    "--PDesktop": "$xl",
+  },
+};
