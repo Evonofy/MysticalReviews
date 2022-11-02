5b86747f4706c4c145a9495ffd1e64c8a5693791
VitorGouveia <vitorneves.gouveia10@gmail.com>
2022-11-01 22:17:27 -0300

    fix: match accent base lightness with figma's

--- src/stitches.config.ts
+++ src/stitches.config.ts
@@ -9,7 +9,7 @@ export const { styled, config, theme, getCssText } = createStitches({
   theme: {
     colors: {
       accentLight: "hsl(185, 100%, 70%)",
-      accentBase: "hsl(185, 100%, 35%)",
+      accentBase: "hsl(185, 100%, 40%)",
       accentDark: "hsl(185, 60%, 50%)",
       accentDarkest: "hsl(185, 45%, 20%)",
       secondaryLight: "hsl(240, 100%, 70%)",
