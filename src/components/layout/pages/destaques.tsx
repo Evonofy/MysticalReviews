08ac6e0dcdae3f396e7acfd57b5c9ce9a313ab62
VitorGouveia <vitorneves.gouveia10@gmail.com>
2022-12-19 09:54:20 -0300

    done

---
+++ src/components/layout/pages/destaques.tsx
@@ -0,0 +1,47 @@
+import React, { FunctionComponent } from "react";
+
+import { Section } from "@/components/layout/section";
+import { Card, CardProps } from "@/components/Card";
+import { CardSlider } from "@/components/card-slider";
+import { Title } from "@/components/Title";
+
+export const Destaques: FunctionComponent<{ cards: CardProps[] }> = ({
+  cards,
+}) => {
+  const allCategories = cards
+    .map(({ genres }) => genres)
+    .reduce((acc, curr) => [...new Set(acc.concat(curr))], []);
+
+  return (
+    <>
+      {allCategories.map((category) => (
+        <Section key={category} css={{ "@desktop": { display: "none" } }}>
+          <Title>{category}</Title>
+
+          <CardSlider
+            cards={cards.filter((card) => card.genres.includes(category))}
+          />
+        </Section>
+      ))}
+
+      {allCategories.map((category) => (
+        <Section
+          key={category}
+          css={{ display: "none", "@desktop": { display: "flex" } }}
+        >
+          <Title>{category}</Title>
+
+          <ul style={{ display: "flex", flexDirection: "column", gap: "12px" }}>
+            {cards
+              .filter((card) => card.genres.includes(category))
+              .map(({ ...props }) => (
+                <li key={props.title}>
+                  <Card css={{ img: { height: "450px" } }} {...props} />
+                </li>
+              ))}
+          </ul>
+        </Section>
+      ))}
+    </>
+  );
+};
