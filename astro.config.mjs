d60e8eab2c72ec6c37f6bca638c477a391bfcf2f
VitorGouveia <vitorneves.gouveia10@gmail.com>
2022-12-29 16:21:49 -0300

    add node ssr

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,8 +2,18 @@ import { defineConfig } from "astro/config";
 import react from "@astrojs/react";
 
 // https://astro.build/config
+import node from "@astrojs/node";
+
+// https://astro.build/config
+// import vercel from "@astrojs/vercel/serverless";
+
+// https://astro.build/config
 export default defineConfig({
   integrations: [react()],
   base: "MysticalReviews",
+  output: "server",
   publicDir: "public",
+  adapter: node({
+    mode: "standalone",
+  }),
 });
